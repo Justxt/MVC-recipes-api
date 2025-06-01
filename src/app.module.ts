562a637b8c59701dc93de6acf16ad0ccaf026cca
Justxt <jostinmatt@gmail.com>
2025-06-01 13:29:32 -0500

    feat(app.m): Se agrega entidades y se modifica el entities: ** para que las encuentre

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { IngredientsModule } from './ingredients/ingredients.module';
+import { RecipesModule } from './recipes/recipes.module';
 
 @Module({
   imports: [
@@ -12,17 +14,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        host: configService.get<string>('HOST'),
-        port: parseInt(configService.get<string>('PORT')!),
+        host: configService.get<string>('HOST') || 'localhost',
+        port: parseInt(configService.get<string>('PORTDB') || '5432'),
         username: configService.get<string>('USER'),
         password: configService.get<string>('PASS'),
         database: configService.get<string>('DB_NAME'),
-        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
         synchronize: true,
         autoLoadEntities: true,
       }),
       inject: [ConfigService],
     }),
+    RecipesModule,
+    IngredientsModule,
   ],
   controllers: [],
   providers: [],
