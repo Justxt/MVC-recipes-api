6e67817cc5a30d9c39e59946f221325d0304d267
Justxt <justxt0@gmail.com>
2025-06-29 20:17:00 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -17,13 +17,12 @@ async function bootstrap() {
   // Agregar el ClassSerializerInterceptor para manejar @Exclude()
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
-  await app.listen(process.env.PORT ?? 3000);
-
-  app.enableCors({
+    app.enableCors({
     origin: ['http://localhost:5173', 'https://recipes-xi-lake.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+  await app.listen(process.env.PORT ?? 3000);
 }
 
 bootstrap();
