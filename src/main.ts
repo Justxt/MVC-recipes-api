9b657c316d2ede2fa84eab7902980f0524c608c6
Justxt <justxt0@gmail.com>
2025-06-29 19:42:33 -0500

    CryptoUUID fixed

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+import * as nodeCrypto from 'crypto';
+(global as any).crypto = (global as any).crypto || nodeCrypto;
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe, ClassSerializerInterceptor } from '@nestjs/common';
@@ -14,7 +17,6 @@ async function bootstrap() {
     }),
   );
 
-  // Agregar el ClassSerializerInterceptor para manejar @Exclude()
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
   await app.listen(process.env.PORT ?? 3000);
